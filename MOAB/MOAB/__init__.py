30e250d8bceb4168f7ac66b5cda8d06dfea13bee
Rob Stickles <140325876+rstickles16@users.noreply.github.com>
2024-07-13 07:52:56 -0400

    Delete MOAB directory

--- MOAB/MOAB/__init__.py
+++
@@ -1,9 +0,0 @@
-import StratLab as sl
-from . import conditions
-from . import holdings
-
-
-bt = sl.Backtest(to_excel=True)
-conditions.add_moab_conditions(bt)
-holdings.add_moab_holdings(bt)
-bt.run()
